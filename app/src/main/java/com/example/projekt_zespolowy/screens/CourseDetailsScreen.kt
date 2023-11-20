db3e4411a69cf4ced985a0b7859ff0951ed7694f
RadoslawStanczak <radoslaw.stanczak2001@gmail.com>
2023-11-20 13:02:57 +0100

    1.3.1 detale kursu

--- app/src/main/java/com/example/projekt_zespolowy/screens/CourseDetailsScreen.kt
+++ app/src/main/java/com/example/projekt_zespolowy/screens/CourseDetailsScreen.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ElevatedCard
@@ -20,6 +23,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
@@ -31,8 +35,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -80,32 +82,20 @@ fun CourseDetails(onClick: (String) -> Unit, context: Context) {
                         containerColor = MaterialTheme.colorScheme.tertiaryContainer
                     )
                 ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(10.dp),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(6.dp),
-                            text = "// Miejsce na dane",
-                            fontSize = 23.sp,
-                            style = TextStyle(fontWeight = FontWeight.ExtraBold)
-                        )
-                    }
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .height(500.dp)
-                            .padding(8.dp)
+                            .height(550.dp)
+                            .padding(10.dp)
                             .clickable {
                                 onClick("detaleKursu")
-                                Toast.makeText(
-                                    context,
-                                    "Clicked!",
-                                    Toast.LENGTH_SHORT
-                                ).show()
+                                Toast
+                                    .makeText(
+                                        context,
+                                        "Clicked!",
+                                        Toast.LENGTH_SHORT
+                                    )
+                                    .show()
                             }
                             .background(
                                 MaterialTheme.colorScheme.surface,
@@ -129,12 +119,34 @@ fun CourseDetails(onClick: (String) -> Unit, context: Context) {
                                 fontSize = 14.sp,
                                 textAlign = TextAlign.Center,
                             )
-                            Text(text = "Nazwa_kursu")
                             Text(text = "Organizator kursu")
                             Text(text = "Data_rozpoczęcia")
                             Text(text = "Data_zakończenia")
                             Text(text = "Cena_za_udział")
 
+                        }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        OutlinedButton(
+                            onClick = {
+
+                            }
+                        ) {
+                            Text(text = "Zamknij")
+                        }
+                        Spacer(modifier = Modifier.fillMaxHeight().width(80.dp))
+                        OutlinedButton(
+                            onClick = {
+
+                            }
+                        ) {
+                            Text(text = "Zapisz się")
                         }
                     }
                 }
